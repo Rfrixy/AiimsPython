ff77ac649bdabc4d0c27efe67c8f829a8c341b28
diveshnn <ddc.divesh@gmail.com>
2018-01-27 05:32:22 +0530

    Presentation ready, added  links

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const ipdata = require('./ipdata.js');
 
 const port = process.env.PORT || 3000;
 
+
 var app = express();
-hbs.registerPartials(__dirname+ '/views/partials')
+hbs.registerPartials(__dirname+ '/views/partials');
+
 hbs.registerHelper('getCurrentYear', ()=>{
   return new Date().getFullYear();
 });
@@ -15,10 +17,14 @@ hbs.registerHelper('getCurrentYear', ()=>{
 hbs.registerHelper('screamIt', (text)=>{
   return text.toUpperCase();
 });
+
 app.set('view engine', 'hbs');
 
-app.use((req, res, next)=>{ // middleware function
 
+app.use(express.static(__dirname + "/public"));
+
+
+app.use((req, res, next)=>{ // middleware function
   next();
 });
 
@@ -29,34 +35,31 @@ app.get('/', (req, res)=>{
   var log = `${now}: ${req.method} ${req.url} \n ip: ${req.ip}`;
   var data = req.headers['user-agent'];
   var log2 = `ip: ${req.ip}`
-
-  ipdata.ipData(req.ip,(errorMessage,results)=>{
-    if(errorMessage){
-      console.log(errorMessage);
-    }else{
-      console.log(log + '\n' + data + '\n' + results.as + " | " + results.city + '\n');
-    }
-    console.log("=============================");
-  });
-
-
-
-  res.render('home.hbs',{
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
+  console.log(data);
+  console.log("=============================");
+  // ipdata.ipData(req.ip,(errorMessage,results)=>{
+  //   if(errorMessage){
+  //     console.log(errorMessage);
+  //   }else{
+  //     console.log(log + '\n' + data + '\n' + results.as + " | " + results.city + '\n');
+  //   }
+  //   console.log("=============================");
+  // });
+  res.render('essentials.hbs',{
+    pageTitle: 'Essentials'
   })
 
 });
 
-app.get('/about', (req,res)=>{
-  res.render('about.hbs',{
-    pageTitle: 'About Page',
+app.get('/setup', (req,res)=>{
+  res.render('setup.hbs',{
+    pageTitle: 'Setup',
   });
 });
 
-app.get('/projects', (req,res)=>{
-  res.render('projects.hbs',{
-    pageTitle: 'My projects',
+app.get('/resources', (req,res)=>{
+  res.render('resources.hbs',{
+    pageTitle: 'Resources',
   });
 });
 
